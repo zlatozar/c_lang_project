40d6db798de1c78bc77c2aef8864401111338ff4
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2021-04-14 17:13:03 +0300

    Add binary tree

--- src/libs/data_structs/binary_tree.c
+++ src/libs/data_structs/binary_tree.c
@@ -1,6 +1,7 @@
 #include "data_structs/binary_tree.h"
 
 #include "lang/memory.h"
+#include "lang/assert.h"
 
 struct tree_node {
   Generic_T datapointer;
@@ -50,4 +51,131 @@ bool
 BinTree_is_empty(BinTree_T btree)
 {
   return btree == NULL;
+}
+
+/* Build like a LEGO. */
+void
+BinTree_make_root(BinTree_T* p_btree, Generic_T data, BinTree_T left, BinTree_T right)
+{
+  if (!BinTree_is_empty(*p_btree)) {
+    Require(0 && "Passed tree is not empty");
+  }
+
+  __allocate_tree_node(p_btree, data);
+
+  LEFT(*p_btree) = left;
+  RIGHT(*p_btree) = right;
+}
+
+static bool
+__preorder_traverse(BinTree_T btree, bool (*apply_fn)(Generic_T))
+{
+  Require(apply_fn);
+
+  bool result;
+
+  if (BinTree_is_empty(btree))
+  { return true; }
+
+  /* First process data. */
+  result = apply_fn(DATA(btree));
+
+  if (result) {
+    result = __preorder_traverse(LEFT(btree), apply_fn);
+  }
+
+  if (result) {
+    result = __preorder_traverse(RIGHT(btree), apply_fn);
+  }
+
+  return result;
+}
+
+static bool
+__inorder_traverse(BinTree_T btree, bool (*apply_fn)(Generic_T))
+{
+  Require(apply_fn);
+
+  bool result;
+
+  if (BinTree_is_empty(btree))
+  { return true; }
+
+  result = __inorder_traverse(LEFT(btree), apply_fn);
+
+  /* Process data between. */
+  if (result) {
+    result = apply_fn(DATA(btree));
+  }
+
+  if (result) {
+    result = __inorder_traverse(RIGHT(btree), apply_fn);
+  }
+
+  return result;
+}
+
+static bool
+__postorder_traverse(BinTree_T btree, bool (*apply_fn)(Generic_T))
+{
+  Require(apply_fn);
+
+  bool result;
+
+  if (BinTree_is_empty(btree))
+  { return true; }
+
+  result = __postorder_traverse(LEFT(btree), apply_fn);
+
+  if (result) {
+    result = __postorder_traverse(RIGHT(btree), apply_fn);
+  }
+
+  /* Process data at last. */
+  if (result) {
+    result = apply_fn(DATA(btree));
+  }
+
+  return result;
+}
+
+bool
+BinTree_traverse(BinTree_T btree, bool (*apply_fn)(Generic_T), order_et order)
+{
+  switch (order) {
+    case PRE_ORDER:
+      return __preorder_traverse(btree, apply_fn);
+
+    case IN_ORDER:
+      return __inorder_traverse(btree, apply_fn);
+
+    case POST_ORDER:
+      return __postorder_traverse(btree, apply_fn);
+
+    default:
+      Require(0 && "Unknown traverse order.");
+  }
+
+  return false;
+}
+
+void
+BinTree_destroy(BinTree_T* p_btree, free_data_FN free_data_fn)
+{
+  if (!BinTree_is_empty(*p_btree)) {
+    BinTree_destroy(&LEFT(*p_btree), free_data_fn);
+    BinTree_destroy(&RIGHT(*p_btree), free_data_fn);
+
+    if (free_data_fn != NULL) {
+      free_data_fn(DATA(*p_btree));
+    }
+
+    __free_tree_node(p_btree);
+  }
+}
+
+void
+BinTree_free(BinTree_T* p_btree)
+{
+  BinTree_destroy(p_btree, NULL);
 }
--- src/libs/data_structs/include/data_structs/binary_tree.h
+++ src/libs/data_structs/include/data_structs/binary_tree.h
@@ -10,7 +10,7 @@
 typedef struct tree_node btnode_t;
 typedef btnode_t* BinTree_T;
 
-typedef enum { PRE_ORDER, IN_ORDER, P0ST_0RDER } order_et;
+typedef enum { PRE_ORDER, IN_ORDER, POST_ORDER } order_et;
 
 /**
  * Initialize an empty tree.
@@ -21,5 +21,23 @@ extern BinTree_T BinTree_new(void);
  * Check if given tree is empty.
  */
 extern bool BinTree_is_empty(BinTree_T btree);
+
+/**
+ * Allocate a new node and make that the root of a tree.
+ */
+extern void BinTree_make_root(BinTree_T* p_btree, Generic_T data, BinTree_T left, BinTree_T right);
+
+/**
+ * Traverse a tree in preorder, postorder, or inorder.
+ */
+extern bool BinTree_traverse(BinTree_T btree, bool (*apply_fn)(Generic_T), order_et order);
+
+/*
+ * Delete an entire tree (uses a postorder traversal), calling
+ * `free_data_fn` with the data stored at each node.
+ */
+extern void BinTree_destroy(BinTree_T* p_btree, free_data_FN free_data_fn);
+
+extern void BinTree_free(BinTree_T* p_btree);
 
 #endif  /* DATA_STRUCTS_BINARY_TREE_H */
